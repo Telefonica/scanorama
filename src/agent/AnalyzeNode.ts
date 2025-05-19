8e590f52b837b97dc6cd8624a9eb6f057f0afd57
alexgara <alexgara.git@simonides.anonaddy.com>
2025-05-19 12:43:39 +0200

    Add mandatory license header at the source code

--- src/agent/AnalyzeNode.ts
+++ src/agent/AnalyzeNode.ts
@@ -1,3 +1,12 @@
+/**
+© 2025 Telefónica Innovación Digital S.L.
+
+This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with this library; if not, see <https://www.gnu.org/licenses/>.
+*/
 import { GraphState, GraphUpdate } from './Agent';
 import { AnalysisResult, MCPTool } from '../types/State';
 import { BaseChatModel } from '@langchain/core/language_models/chat_models';
